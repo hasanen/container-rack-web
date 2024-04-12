c30e2ec965448b985295d28bb584b6402d5a153f
Joni Hasanen <joni@pieceofcode.com>
2024-04-12 10:25:58 +0300

    Update scale and expand menu always

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,7 @@ struct OrganizerInputs {
 pub fn App() -> impl IntoView {
     view! {
         <Router>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <nav class="navbar navbar-expand navbar-dark bg-dark">
                 <div class="container">
                     <a class="navbar-brand">Container rack</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
